bd78b86d448ab73f16db090462ab0e383ff1068c
Aniket Chavan <mail.anik3t@gmail.com>
2019-11-05 20:38:07 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include "ymalloc.h"
 
 int main(void) 
 {
 	int i,sum=0;
+	
 	int* x= (int*) ymalloc(10*sizeof(int));
 
 	printf("Enter elements: ");
-   	for(i = 0; i < 10; ++i)
+   	for(i = 0; i < 3; ++i)
     	{
+	printf("Address : %p\n",x+i);
         scanf("%d", x + i);
         sum += *(x + i);
     	}
 
-	printf("Sum = %d", sum);
+	printf("Sum = %d\n", sum);
+	
 	yfree(x);
-
+	
+	
 
 	return 0;
 }
